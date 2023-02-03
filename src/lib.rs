ecb344b4bb2d3868940960e94f0911af591269ea
yinkakun <yinkakun@gmail.com>
2023-02-03 19:16:00 +0100

    public with struct and enum behaviours

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,41 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn cook_order() {}
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+}
+
+fn deliver_order() {}
+
 fn eat_at_restaurant() {
+    let appetizer_1 = back_of_house::Appetizer::Salad;
+    let mut meal = back_of_house::Breakfast::summer("Bread");
+    meal.toast = String::from("Yolo");
+
     // absolute path
     crate::front_of_house::hosting::add_to_wait_list();
 
